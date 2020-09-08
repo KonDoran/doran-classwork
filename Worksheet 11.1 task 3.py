34f4e667bde5712d87821a8f9972bd0d8320ffdf
KonDoran <70944904+KonDoran@users.noreply.github.com>
2020-09-08 14:24:25 +0100

    Update Worksheet 11.1 task 3.py

--- Worksheet 11.1 task 3.py
+++ Worksheet 11.1 task 3.py
@@ -9,3 +9,7 @@ print("Please enter the total number of litres taken to fill the tank.")
 litres = float(input())
 gallonsmile = (litres*gallon)/milestravelled 
 print("Your car consumes",gallonsmile,"gallons per mile.")
+
+#i) the gallon can be defined as a constant so that it can be easier to write than the number needed in a calculation
+#ii)the miles can be integers as the car's mileage only increases by integer values
+#iii) the number of litres needed to fill the tank can be a decimal as the tank is not always going to take an integer amount of litres to fill.
