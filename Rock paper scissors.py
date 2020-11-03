e0d9ef3be12c3e250d4b9c71c33ab846a2984030
KonDoran <70944904+KonDoran@users.noreply.github.com>
2020-11-03 10:18:29 +0000

    Delete Rock paper scissors.py

--- Rock paper scissors.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-result = ["win", "lose","draw","win","loss"]
-choice = ["Rock", "Paper", "Scissors"]
-computer = randint(1,3)
-print(result[(int(input("Enter 1 for Rock, 2 for Paper, 3 for Scissors."))-computer+2)], "Computer chose:",choice[computer-1])
-
-
