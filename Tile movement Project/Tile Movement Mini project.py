d0b78ff3dc5259bc0f05e593557950089f018626
KonDoran <70944904+KonDoran@users.noreply.github.com>
2020-11-24 08:36:37 +0000

    Update Tile Movement Mini project.py

--- Tile movement Project/Tile Movement Mini project.py
+++ Tile movement Project/Tile Movement Mini project.py
@@ -9,7 +9,7 @@ RED = (255, 0, 0)
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (1000, 1000)
+size = (1200, 1000)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("Tile Movement Game")
@@ -39,13 +39,13 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.changespeed(-20,0)
+            self.changespeed(-10,0)
         if keys[pygame.K_d]:
-            self.changespeed(20,0)
+            self.changespeed(10,0)
         if keys[pygame.K_w]:
-            self.changespeed(0,-20)
+            self.changespeed(0,-10)
         if keys[pygame.K_s]:
-            self.changespeed(0,20)
+            self.changespeed(0,10)
         #end if
         self.move(self.speed_x,self.speed_y)
         self.speed_x = 0
@@ -55,7 +55,7 @@ class Player(pygame.sprite.Sprite):
         #move along x
         self.rect.x += self.speed_x
 
-        wallcollision = pygame.sprite.spritecollide(self,outsidewall_group, False)
+        wallcollision = pygame.sprite.spritecollide(self,wall_group, False)
         for wall in wallcollision:
             if self.speed_x > 0:
                 self.rect.right = wall.rect.left
@@ -66,7 +66,7 @@ class Player(pygame.sprite.Sprite):
     
         #move up the screen
         self.rect.y += self.speed_y
-        wallcollision = pygame.sprite.spritecollide(self, outsidewall_group, False)
+        wallcollision = pygame.sprite.spritecollide(self, wall_group, False)
         for wall in wallcollision:
             if self.speed_y > 0:
                 self.rect.bottom = wall.rect.top
@@ -94,22 +94,79 @@ class Wall(pygame.sprite.Sprite):
         pass
     #end procedure
 
+class InnerWall(Wall):
+
+        #wallhits = durability
+    pass
+    #endprocedure
+    def update(self):
+        pass
+        
+
+level1 = [
+   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+   
+    ]
+
+
+            
+
 # Create a list of all sprites
 all_sprites_group = pygame.sprite.Group()
 outsidewall_group = pygame.sprite.Group()
+wall_group = pygame.sprite.Group()
+innerwall_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 #Create an instance of player
-player = Player(BLACK, 25, 25,500,500)
+player = Player(BLACK, 40, 40,500,500)
 all_sprites_group.add(player)
 player_group.add(player)
 
-
-for i in range(0,25):
-    for j in range(0,25):
-        if (i == 0 or i == 24) or (j==0 or j == 24):
+for j in range(0,len(level1)):
+    for i in range(0,len(level1[j])):
+        char = level1[i][j]
+        if char == 1:
             outsidewall = Wall(RED,40,40,i*40, j*40)
             all_sprites_group.add(outsidewall)
+            wall_group.add(outsidewall)
             outsidewall_group.add(outsidewall)
+        if char == 2:
+            innerwall = InnerWall(RED,40,40,i*40, j*40)
+            all_sprites_group.add(innerwall)
+            wall_group.add(innerwall)
+            innerwall_group.add(innerwall)
+
+#for i in range(0,25):
+ #   for j in range(0,25):
+      #  if (i == 0 or i == 24) or (j==0 or j == 24):
+      #      outsidewall = Wall(RED,40,40,i*40, j*40)
+      #      all_sprites_group.add(outsidewall)
+      #      outsidewall_group.add(outsidewall)
+            
 #Loop until the user clicks the close button.
 done = False
  
