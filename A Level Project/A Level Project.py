177b24d65022afbadfd5dfd8050f1b06bbda52b7
KonDoran <70944904+KonDoran@users.noreply.github.com>
2021-01-25 08:45:54 +0000

    Update A Level Project.py

--- A Level Project/A Level Project.py
+++ A Level Project/A Level Project.py
@@ -1,7 +1,11 @@
 import pygame
 import random
+import os
 
 
+
+current_path = os.path.dirname(__file__)#where this file is located
+image_path = os.path.join(current_path, 'images')
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -252,5 +256,292 @@ class Game(object):
         # update the screen with what we've drawn
         pygame.display.flip()
     #end procedure
+
+
+class Player(pygame.sprite.Sprite):
+    #define the constructor for the player
+    speed_x = 0
+    speed_y = 0
+    def __init__(self,color , width, height, x, y, health, score, money, gamekeys):
+        #call sprite constructor
+        super().__init__()
+        #create a sprite
+        self.image = pygame.image.load(os.path.join(image_path, 'player.png'))
+        #set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.health = health
+        self.score = score
+        self.money = money
+        self.gamekeys = gamekeys
+        self.rect.x = x
+        self.rect.y = y
+        self.directionx = 0
+        self.directiony = 5
         
+    #end procedure
+    def gethealth(self):
+        return self.health
+    #endprocedure
+
+    def sethealth(self, newhealth):
+        self.health = newhealth
+    #endfunction
+
+    def getscore(self):
+        return self.score
+    #endprocedure
+
+    def setscore(self, score):
+        self.score = score
+    #endfunction
+
+    def getkeys(self):
+        return self.gamekeys
+    #endprocedure
+
+    def setkeys(self, keys):
+        self.gamekeys = keys
+    #endfunction
+
+    def getmoney(self):
+        return self.money
+    #endprocedure
+
+    def setmoney(self, money):
+        self.money = money
+    #endfunction
+
+    def changespeed(self, x, y):
+        self.speed_x += x
+        self.speed_y += y
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.changespeed(-4,0)
+            self.directionx = -6
+            self.directiony = 0
+        if keys[pygame.K_d]:
+            self.changespeed(4,0)
+            self.directionx = 6
+            self.directiony = 0      
+        if keys[pygame.K_w]:
+            self.changespeed(0,-4)
+            self.directionx = 0
+            self.directiony = -6
+        if keys[pygame.K_s]:
+            self.changespeed(0,4)
+            self.directionx = 0
+            self.directiony = 6
+        if keys[pygame.K_SPACE]:
+            if len(game.bullet_group) == 0:
+                #print("held down")
+                bullet = Bullet(RED, self.directionx, self.directiony)
+                game.bullet_group.add(bullet)
+                game.all_sprites_group.add(bullet)
+                
+        #end if
+        self.move(self.speed_x,self.speed_y)
+        self.speed_x = 0
+        self.speed_y = 0
+        player_hit_group = pygame.sprite.groupcollide(game.player_group, game.enemy_group, False, False)
+        for self in player_hit_group:
+            self.health -= 2
+            if self.health < 1:
+                game.score += 100
+                self.kill()
+    #end procedure
+
+    def move(self, speedx, speedy):
+        #move along x
+        self.rect.x += self.speed_x
+
+        wallcollision = pygame.sprite.spritecollide(self,game.wall_group, False)
+        for wall in wallcollision:
+            if self.speed_x > 0:
+                self.rect.right = wall.rect.left
+            else:
+                self.rect.left = wall.rect.right    
+        #move the player up and down the screen
+        self.rect.y += self.speed_y
+        #check for collision
+        wallcollision = pygame.sprite.spritecollide(self, game.wall_group, False) 
+        for wall in wallcollision:
+            #if there is a collision while moving up then set the speed to 0
+            if self.speed_y > 0: 
+                #i
+                self.rect.bottom = wall.rect.top
+            else:
+                self.rect.top = wall.rect.bottom
+    #end procedure    
+#end class
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, color, speedx, speedy):
+        #Call the sprite constructor
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(image_path, 'fireball.png'))
+        self.rect = self.image.get_rect()
+        self.speedx = speedx
+        self.speedy = speedy
+        self.rect.y = game.player.rect.y + 10
+        self.rect.x = game.player.rect.x  + 10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if pygame.sprite.groupcollide(game.bullet_group, game.wall_group, True, False) == True:
+            self.remove()
+        self.rect.x += self.speedx
+        if pygame.sprite.groupcollide(game.bullet_group, game.wall_group, True, False) == True:
+            self.remove()
+        
+class Key(pygame.sprite.Sprite):
+    def __init__(self,color,x,y):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(image_path, 'silverkey.png'))
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+    def update(self):
+        key_hit_group = pygame.sprite.groupcollide(game.key_group, game.player_group, True, False)
+        for self in key_hit_group:
+            game.player.gamekeys += 1
+            game.score += 50
+            if  game.player.gamekeys == ((2*(game.level+1)) + 1):
+                game.portal = Portal(PURPLE, 23*40, 23*40)
+                game.all_sprites_group.add(game.portal)
+                game.portal_group.add(game.portal)
+
+
+
+
+
+class Portal(pygame.sprite.Sprite):
+    def __init__(self,color,x,y):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(image_path, 'portal2.png'))
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+    #end procedure    
+    def update(self):
+        pass
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, color, width, height, x, y, posx, posy):
+        #call sprite constructor
+        super().__init__()
+        #create a sprite
+        self.image = pygame.image.load(os.path.join(image_path, 'wall.png'))
+        #set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.positionx = posx
+        self.positiony = posy
+    #end procedure
+    def update(self):
+        pass
+    #end procedure
+
+    def getpos(self):
+        return [self.positionx, self.positiony]
+
+class InnerWall(Wall):
+
+        #wallhits = durability
+    pass
+    #endprocedure
+    def update(self):
+        pass
+        
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, direction, width, height, x, y, health):
+        #call sprite constructor
+        super().__init__()
+        #create a sprite
+        self.image = pygame.image.load(os.path.join(image_path, 'enemy.png'))
+        #set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.health = health
+        self.direction = direction
+        self.move = 5
+        
+    #end procedure
+    def update(self):
+        
+        self.MOVE()
+        enemy_hit_group = pygame.sprite.pygame.sprite.groupcollide(game.enemy_group, game.bullet_group, False, True)
+        for self in enemy_hit_group:
+            self.health -= 20
+            #print(self.health)
+            if self.health < 1:
+                game.score += 100
+                gamekey = Key(PINK, self.rect.x + 2, self.rect.y + 9)
+                game.all_sprites_group.add(gamekey)
+                game.key_group.add(gamekey)
+                self.kill()
+                
+    #end procedure
+
+
+    def MOVE(self):
+        if self.direction % 2 == 0:
+            self.rect.x += self.move
+            wallcollision = pygame.sprite.groupcollide(game.enemy_group,game.wall_group, False,False)
+            for wall in wallcollision:
+                if self.move > 0:
+                    self.rect.right = wall.rect.left
+                    self.move = self.move * -1
+                else:
+                    self.rect.left = wall.rect.right    
+                    self.move = self.move * -1
+    
+    
+    
+        #move the player up and down the screen
+        else:
+            self.rect.y += self.move
+            #check for collision
+            wallcollision = pygame.sprite.groupcollide(game.enemy_group, game.wall_group, False, False) 
+            for wall in wallcollision:
+                #if there is a collision while moving up then set the speed to 0
+                if self.move > 0: 
+                    self.rect.bottom = wall.rect.top
+                    self.move = self.move * -1
+                else:
+                    self.rect.top = wall.rect.bottom
+                    self.move = self.move * -1
+
+
+    def gethealth(self):
+        return self.health
+    #endprocedure
+
+    def sethealth(self, newhealth):
+        self.health = newhealth
+    #endfunction
+
+
+game = Game()
+# -------- Main Program Loop -----------
+while not done:
+        # --- Main event loop
+    done = game.eventprocess()
+    
+        # --- Game logic should go here
+    game.runlogic()
+
+        #draw the screen
+    game.display(screen)
+        
+        # --- Limit to 60 frames per second
+    clock.tick(60)
+    
+    # Close the window and quit.
+pygame.quit()
+       
         
